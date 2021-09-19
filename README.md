cf02f5ee6b39a29e83c6be0d743168f7f2e62c79
Humayun Javed <arnetraff@gmail.com>
2021-09-19 16:21:06 +0500

    Removed backend from main

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rb_hackfest_swvl_3
 For Remotebase Hackfest SWVL#3 Challenge
+
+
+Switch to master branch for code.
--- backend/util.js
+++
@@ -1,7 +0,0 @@
-export function rejectWithError(res, errorMessage) {
-  res.json({ ok: false, error: { msg: errorMessage } });
-}
-
-export function respondWithData(res, data) {
-  res.json({ ok: true, data });
-}
